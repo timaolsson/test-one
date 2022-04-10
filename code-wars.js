e8528aecae0f2fa63503591118f4519466095df5
timaolsson <timothyaolsson@outlook.com>
2022-04-10 08:30:52 -0400

    Daily kata practice

--- code-wars.js
+++ code-wars.js
@@ -1,3 +1,12 @@
+//How good are you really?
+function betterThanAverage(classPoints, yourPoints) {
+  //return true if yourPoints is greater than the class average
+  let sum = 0
+  classPoints.forEach(x => sum += x)
+  let classAvg = sum / classPoints.length
+  return yourPoints > classAvg
+}
+
 //Convert a string to an array
 function stringToArray(string){
     return string.split(' ')
