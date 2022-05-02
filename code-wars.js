d04e13ae62119aef9fca0fdee71241853f03ae5f
timaolsson <timothyaolsson@outlook.com>
2022-05-01 20:48:09 -0400

    daily kata #2

--- code-wars.js
+++ code-wars.js
@@ -1,3 +1,12 @@
+//Grasshopper-Summation
+var summation = function (num) {
+  let sum = 0
+  for(let i = 0; i <= num; i++){
+    sum += i
+  }
+  return sum
+}
+
 //String repeat
 function repeatStr (n, s) {
   let answer = ''
